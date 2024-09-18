c7f24c835e839c16cdab8def812caad4fe349565
bmaggs1 <bmaggs@asu.edu>
2024-09-17 22:30:22 -0700

    added name to user data for multiple users and hash function so user name isnt directly stored in the files

--- start.py
+++ start.py
@@ -1,5 +1,21 @@
-import os, json
+import os, json, hashlib
 import Nutrition
+
+nutrition_goals = {
+    '1': 'Lose 1 pound a week',
+    '2': 'Lose 0.5 pound a week',
+    '3': 'Maintain weight',
+    '4': 'Gain 0.5 pound a week',
+    '5': 'Gain 1 pound a week'
+}
+activity_levels = {
+    '1': 'Little to no exercise',
+    '2': 'Exercise 1-3 times/week',
+    '3': 'Exercise 4-5 times/week',
+    '4': 'Daily exercise or intense exercise 3-4 times/week',
+    '5': 'Intense exercise 6-7 times/week',
+    '6': 'Very intense exercise daily, or physical job'
+}
 
 class User:
     def calculate_daily_calories(self):
@@ -11,7 +27,8 @@ class User:
         dailyCals = tot_bmr + (200 * (self.nutrition_goal - 3))
         return dailyCals
 
-    def __init__(self, gender, weight, height, age, activity_level, nutritition_goal):
+    def __init__(self, name, gender, weight, height, age, activity_level, nutritition_goal):
+        self.name = name
         self.gender = gender
         self.weight = weight
         self.height = height
@@ -23,6 +40,7 @@ class User:
     def to_dict(self):
         """Convert user data to a dictionary for saving to JSON"""
         return {
+            'name': self.name,
             'gender': self.gender,
             'weight': self.weight,
             'height': self.height,
@@ -34,6 +52,7 @@ class User:
     @staticmethod
     def from_dict(data):
         return User(
+            name = data['name'],
             gender = data['gender'],
             weight = data['weight'],
             height = data['height'],
@@ -42,52 +61,62 @@ class User:
             nutritition_goal = data['nutrition_goal']
         )
 
+def hash_name(name):
+    # Hash the name using SHA-1
+    sha1_hash = hashlib.sha1(name.encode()).hexdigest()
+    # Truncate the hash to 20 characters
+    return sha1_hash[:20]
+
 def save_user_data(user):
-    with open('user_data.json', 'w') as file:
+    hashed_name = hash_name(user.name)
+    with open(f'{hashed_name}_data.json', 'w') as file:
         json.dump(user.to_dict(), file)
     print("User data saved successfully.")
 
-def load_user_data():
-    if os.path.exists('user_data.json'):
-        with open('user_data.json', 'r') as file:
+def load_user_data(name):
+    hashed_name = hash_name(name)
+    if os.path.exists(f'{hashed_name}_data.json'):
+        with open(f'{hashed_name}_data.json', 'r') as file:
             data = json.load(file)
             return User.from_dict(data)
     return None
 
-def get_user_info():
+def get_user_info(name):
     print("Welcome to my nutrition planner!")
     print("To get started please enter some information.")
 
     #get gender to use in equation, only 2 options as it is based
     #on basal metabolic rate and is determined by male or female
-    user_gender = input("Please enter your gender (m/f) : ")
+    user_gender = input("Please enter your gender (m/f):  ")
     while user_gender.lower() not in ['m','f']:
-        user_gender = input("Please enter m or f : ")
+        user_gender = input("Please enter m or f:  ")
+    print()
 
     #get user weight until a valid weight is entered
-    user_weight = input("Please enter your weight in pounds : ")
+    user_weight = input("Please enter your weight in pounds:  ")
     while user_weight.isdigit() == False or int(user_weight) < 0 or int(user_weight) > 600:
-        user_weight = input("Please enter a valid weight: ")
+        user_weight = input("Please enter a valid weight:  ")
     user_weight = int(user_weight) / 2.20462
-    print(user_weight)
+    print()
 
     #get user height until a valid height is entered
-    print("Please enter your height: ")
-    user_height_feet = input("Feet: ")
-    user_height_inches = input("Inches: ")
+    print("Please enter your height:  ")
+    user_height_feet = input("Feet:  ")
+    user_height_inches = input("Inches:  ")
     while user_height_feet.isdigit() == False or user_height_inches.isdigit() == False or (int(user_height_feet) * 12 + int(user_height_inches)) < 0 or (int(user_height_feet) * 12 + int(user_height_inches)) > 150:
-        print("Please enter a valid height: ")
-        user_height_feet = input("Feet: ")
-        user_height_inches = input("Inches: ")
+        print("Please enter a valid height:  ")
+        user_height_feet = input("Feet:  ")
+        user_height_inches = input("Inches:  ")
     user_height = int(user_height_feet) * 12 + int(user_height_inches)
     user_height *= 2.54
-    print(user_height)
+    print()
 
     #get user weight until a valid weight is entered
-    user_age = input("Please enter your age : ")
+    user_age = input("Please enter your age:  ")
     while user_age.isdigit() == False or int(user_age) < 0 or int(user_age) > 100:
-        user_age = input("Please enter a valid age: ")
+        user_age = input("Please enter a valid age:  ")
     user_age = int(user_age)
+    print()
 
     #Calculate basasl metabolic rate using Mifflin-St Jeor Equation
     basal_metabolic_rate = 0
@@ -95,60 +124,53 @@ def get_user_info():
         basal_metabolic_rate = (10 * user_weight) + (6.25 * user_height) - (5 * user_age) + 5
     else:
         basal_metabolic_rate = (10 * user_weight) + (6.25 * user_height) - (5 * user_age) - 161
-    print(f"Your basal metabolic rate is: {basal_metabolic_rate}\n")
 
 
     #calculate total metabolic rate after exercise
-    print("1: Little to no exercise")
-    print("2: Exercise 1-3 times/week")
-    print("3: Exercise 4-5 times/week")
-    print("4: Daily exercise or intense exercise 3-4 times/week	")
-    print("5: Intense exercise 6-7 times/week")
-    print("6: Very intense exercise daily, or physical job")
-    exercise_level = input("Please choose a number that represents your level of activity: ")
+    for num in activity_levels:
+        print(num, activity_levels.get(num))
+    exercise_level = input("Please choose a number that represents your level of activity:  ")
     while exercise_level.isdigit() == False or int(exercise_level) > 6 or int(exercise_level) < 1:
-        exercise_level = input("Please choose a number 1-6: ")
+        exercise_level = input("Please choose a number 1-6:  ")
     exercise_level = int(exercise_level)
-
     total_BMR = basal_metabolic_rate * (((1.9-1.2)/5) * (exercise_level-1) + 1.2)
-
-    print(f"Your total basal metabolic rate is: {total_BMR}\n")
+    print()
 
     #Prompt the user for their plan of action moving forward.
-    print("Please select your nutrition goal:")
-    print("1: Lose 1 pound a week")
-    print("2: Lose half a pound a week")
-    print("3: Maintain weight")
-    print("4: Gain half a pound a week")
-    print("5: Gain 1 pound a week")
-    nutrition_goal = input("Please enter your age : ")
+    for num in nutrition_goals:
+        print(num, nutrition_goals.get(num))
+    nutrition_goal = input("Please enter your preferred nutrition goal:  ")
     while nutrition_goal.isdigit() == False or int(nutrition_goal) < 1 or int(nutrition_goal) > 5:
-        nutrition_goal = input("Please enter a valid choice: ")
+        nutrition_goal = input("Please enter a valid choice:  ")
     nutrition_goal = int(nutrition_goal)
-
     goal_cals = total_BMR + (200 * (nutrition_goal - 3))
+    user = User(name, user_gender, user_weight, user_height, user_age, exercise_level, nutrition_goal)
+    print()
 
-    print(f"Your daily calorie intake should be {goal_cals} calories.")
 
-    user = User(user_gender, user_weight, user_height, user_age, exercise_level, nutrition_goal)
+    print(f"Your basal metabolic rate is: {basal_metabolic_rate}")
+    print(f"Your total basal metabolic rate is: {total_BMR}")
+    print(f"Your daily calorie intake should be {goal_cals} calories.\n")
+
 
     return user
 
 def main():
-    user = load_user_data()
+    name = input('Welcome!\nPlease enter your name to get started:  ')
+    name = name.lower()
+    user = load_user_data(name)
 
     if not user:
-        user = get_user_info()
+        user = get_user_info(name)
         save_user_data(user)
     else:
         print(f"Welcome back! Here are your stored details:")
-        print(f"Gender: {user.gender}")
         print(f"Weight: {user.weight} kg")
         print(f"Height: {user.height} cm")
         print(f"Age: {user.age}")
-        print(f"Activity Level: {user.activity_level}")
-        print(f"Nutrition Goal: {user.nutrition_goal}")
-        print(f"Your daily calorie intake should be: {user.daily_calories} kcal/day\n")
+        print(f"Activity Level: {activity_levels.get(user.activity_level)}")
+        print(f"Nutrition Goal: {nutrition_goals.get(user.nutrition_goal)}")
+        print(f"Your current daily calorie intake should be: {user.daily_calories} kcal/day")
 
     Nutrition.get_50_random(user)
 
